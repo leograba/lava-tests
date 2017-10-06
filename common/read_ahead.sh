00795322b25b2397c81aab796ba1d3a58d791831
Leonardo Graboski Veiga <leogveiga@gmail.com>
2017-10-06 10:44:22 -0300

    Added read-ahead test case that should work for all modules

---
+++ common/read_ahead.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -x
+
+case $1 in
+	apalis-imx6) MMC=1024 ; SD=512 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1 ;;
+	apalis-t30) MMC=4096 ; SD=1024 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1 ;;
+	apalis-tk1) MMC=4096 ; SD=1024 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1 ;;
+	colibri-imx6) MMC=1024 ; SD=512 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1  ;;
+	colibri-imx6ull) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
+	colibri-imx7) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
+	colibri-imx7-1gb) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
+	colibri-t30) MMC=2048 ; SD=1024 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1  ;;
+	colibri-t20) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
+	colibri-vf) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
+	*) exit 1 ;;
+esac
+
+# MMC
+
+if [ -d /sys/block/${MMC_MOUNT} ] ; then
+	lava-test-case mmc-exist --result pass
+else lava-test-case mmc-exist --result fail
+fi
+
+if [ `cat /sys/block/${MMC_MOUNT}/queue/read_ahead_kb` -ne ${MMC} ] ; then
+	lava-test-case mmc-read-ahead-size --result fail
+else lava-test-case mmc-read-ahead-size --result pass --measurement ${MMC}
+fi
+
+# SD card
+
+if [ -d /sys/block/${SD_MOUNT} ] ; then
+	lava-test-case sd-card-exist --result pass
+else lava-test-case sd-card-exist --result fail
+fi
+
+if [ `cat /sys/block/${SD_MOUNT}/queue/read_ahead_kb` -ne ${SD} ] ; then
+	lava-test-case sd-card-read-ahead-size --result fail
+else lava-test-case sd-card-read-ahead-size --result pass --measurement ${SD}
+fi
