c4985cb23631baa0e21c1ecccf4261ff1ae2ad51
Leonardo Graboski Veiga <leogveiga@gmail.com>
2017-10-06 11:35:13 -0300

    Fix issue of duplicate read_ahead.sh script while saving file

--- common/sh_scripts/read_ahead.sh
+++ common/sh_scripts/read_ahead.sh
@@ -3,16 +3,36 @@
 set -x
 
 case $1 in
-	apalis-imx6) MMC=1024 ; SD=512 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1 ;;
-	apalis-t30) MMC=4096 ; SD=1024 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1 ;;
-	apalis-tk1) MMC=4096 ; SD=1024 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1 ;;
-	colibri-imx6) MMC=1024 ; SD=512 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1  ;;
-	colibri-imx6ull) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
-	colibri-imx7) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
-	colibri-imx7-1gb) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
-	colibri-t30) MMC=2048 ; SD=1024 ; MMC_MOUNT=mmcblk0 ; SD_MOUNT=mmcblk1  ;;
-	colibri-t20) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
-	colibri-vf) MMC=0 ; SD=0 ; MMC_MOUNT= ; SD_MOUNT= ;;
+	apalis-imx6)
+		MMC=1024 ; SD1=512 ; SD2=512
+		MMC_MOUNT=mmcblk0 ; SD1_MOUNT=mmcblk1 ; SD2_MOUNT=mmcblk2 ;;
+	apalis-t30)
+		MMC=4096 ; SD1=1024 ; SD2=512
+		MMC_MOUNT=mmcblk0 ; SD1_MOUNT=mmcblk1 ; SD2_MOUNT=mmcblk2 ;;
+	apalis-tk1)
+		MMC=4096 ; SD1=1024 ; SD2=512
+		MMC_MOUNT=mmcblk0 ; SD1_MOUNT=mmcblk1 ; SD2_MOUNT=mmcblk2 ;;
+	colibri-imx6)
+		MMC=1024 ; SD1=512 ; SD2=0
+		MMC_MOUNT=mmcblk0 ; SD1_MOUNT=mmcblk1 ; SD2_MOUNT=  ;;
+	colibri-imx6ull)
+		MMC=0 ; SD1=0 ; SD2=0
+		MMC_MOUNT= ; SD1_MOUNT= ; SD2_MOUNT= ;;
+	colibri-imx7)
+		MMC=0 ; SD1=0 ; SD2=0
+		MMC_MOUNT= ; SD1_MOUNT= ; SD2_MOUNT= ;;
+	colibri-imx7-1gb)
+		MMC=0 ; SD1=0 ; SD2=0
+		MMC_MOUNT= ; SD1_MOUNT= ; SD2_MOUNT= ;;
+	colibri-t30)
+		MMC=2048 ; SD1=1024 ; SD2=0
+		MMC_MOUNT=mmcblk0 ; SD1_MOUNT=mmcblk1 ; SD2_MOUNT=  ;;
+	colibri-t20)
+		MMC=0 ; SD1=0 ; SD2=0
+		MMC_MOUNT= ; SD1_MOUNT= ; SD2_MOUNT= ;;
+	colibri-vf)
+		MMC=0 ; SD1=0 ; SD2=0
+		MMC_MOUNT= ; SD1_MOUNT= ; SD2_MOUNT= ;;
 	*) exit 1 ;;
 esac
 
@@ -32,18 +52,34 @@ else
 	fi
 fi
 
-# SD card
+# SD card 1
 
-if [ $SD -eq 0 ] ; then
+if [ $SD1 -eq 0 ] ; then
 	:
 else
-	if [ -d /sys/block/${SD_MOUNT} ] ; then
-		lava-test-case sd-card-exist --result pass
-	else lava-test-case sd-card-exist --result fail
+	if [ -d /sys/block/${SD1_MOUNT} ] ; then
+		lava-test-case sd-card1-exist --result pass
+	else lava-test-case sd-card1-exist --result fail
 	fi
 
-	if [ `cat /sys/block/${SD_MOUNT}/queue/read_ahead_kb` -ne ${SD} ] ; then
-		lava-test-case sd-card-read-ahead-size --result fail
-	else lava-test-case sd-card-read-ahead-size --result pass --measurement ${SD}
+	if [ `cat /sys/block/${SD1_MOUNT}/queue/read_ahead_kb` -ne ${SD1} ] ; then
+		lava-test-case sd-card1-read-ahead-size --result fail
+	else lava-test-case sd-card1-read-ahead-size --result pass --measurement ${SD1}
+	fi
+fi
+
+# SD card 2
+
+if [ $SD2 -eq 0 ] ; then
+	:
+else
+	if [ -d /sys/block/${SD2_MOUNT} ] ; then
+		lava-test-case sd-card2-exist --result pass
+	else lava-test-case sd-card2-exist --result fail
+	fi
+
+	if [ `cat /sys/block/${SD2_MOUNT}/queue/read_ahead_kb` -ne ${SD2} ] ; then
+		lava-test-case sd-card2-read-ahead-size --result fail
+	else lava-test-case sd-card2-read-ahead-size --result pass --measurement ${SD2}
 	fi
 fi
