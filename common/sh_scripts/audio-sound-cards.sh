bf17fd30f6a19edc351f768d595e23878977ddd6
Leonardo Graboski Veiga <leogveiga@gmail.com>
2017-12-28 13:23:04 -0200

    Improved and fixed some bugs for audio-sound-cards.sh. Tested on Colibri iMX6

--- common/sh_scripts/audio-sound-cards.sh
+++ common/sh_scripts/audio-sound-cards.sh
@@ -3,23 +3,26 @@
 set -x
 
 if [ -z $1 ]; then echo "no module name"; exit 1; fi
-source $(echo $1 | tr - _)/resources/audio-sound-cards.sh
+source $(echo $1 | tr - _)/resources/audio-sound-cards-threshold-values.sh
 if [ $? -ne 0 ]; then echo "wrong module ID or name"; exit 1; fi
 
 # Playback: ANALOG, HDMI, USB_PLAY, SPDIF
 # Record: MIC, LINE, USB_REC
 
 DEFAULT="sysdefault:CARD="
+APLAY="$(aplay -l)"
+ARECORD="$(arecord -l)"
+CARDS="$(cat /proc/asound/cards)"
 
 # Analog audio playback
 if [ ! -z "$ANALOG" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $ANALOG) ]; then
+	if [ -n "$(echo "$CARDS" | grep $ANALOG)" ]; then
 		lava-test-case analog-playback-device-exist --result pass
 	else
 		lava-test-case analog-playback-device-exist --result fail
 	fi
 
-	if [ -n $(aplay -l | grep $ANALOG) ]; then
+	if [ -n "$(echo "$APLAY" | grep $ANALOG)" ]; then
 		lava-test-case aplay-analog-playback-device-exist --result pass
 	else
 		lava-test-case aplay-analog-playback-device-exist --result fail
@@ -28,13 +31,13 @@ fi
 
 # HDMI audio playback
 if [ ! -z "$HDMI" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $HDMI) ]; then
+	if [ -n "$(echo "$CARDS" | grep $HDMI)" ]; then
 		lava-test-case hdmi-playback-device-exist --result pass
 	else
 		lava-test-case hdmi-playback-device-exist --result fail
 	fi
 
-	if [ -n $(aplay -l | grep $HDMI) ]; then
+	if [ -n "$(echo "$APLAY" | grep $HDMI)" ]; then
 		lava-test-case aplay-hdmi-playback-device-exist --result pass
 	else
 		lava-test-case aplay-hdmi-playback-device-exist --result fail
@@ -43,13 +46,13 @@ fi
 
 # USB audio playback
 if [ ! -z "$USB_PLAY" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $USB_PLAY) ]; then
+	if [ -n "$(echo "$CARDS" | grep $USB_PLAY)" ]; then
 		lava-test-case usb-playback-device-exist --result pass
 	else
 		lava-test-case usb-playback-device-exist --result fail
 	fi
 
-	if [ -n $(aplay -l | grep $USB_PLAY) ]; then
+	if [ -n "$(echo "$APLAY" | grep $USB_PLAY)" ]; then
 		lava-test-case aplay-usb-playback-device-exist --result pass
 	else
 		lava-test-case aplay-usb-playback-device-exist --result fail
@@ -58,13 +61,13 @@ fi
 
 # SPDIF audio playback
 if [ ! -z "$SPDIF" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $SPDIF) ]; then
+	if [ -n "$(echo "$CARDS" | grep $SPDIF)" ]; then
 		lava-test-case spdif-playback-device-exist --result pass
 	else
 		lava-test-case spdif-playback-device-exist --result fail
 	fi
 
-	if [ -n $(aplay -l | grep $SPDIF) ]; then
+	if [ -n "$(echo "$APLAY" | grep $SPDIF)" ]; then
 		lava-test-case aplay-spdif-playback-device-exist --result pass
 	else
 		lava-test-case aplay-spdif-playback-device-exist --result fail
@@ -73,13 +76,13 @@ fi
 
 # Mic audio record
 if [ ! -z "$MIC" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $MIC) ]; then
+	if [ -n "$(echo "$CARDS" | grep $MIC)" ]; then
 		lava-test-case mic-record-device-exist --result pass
 	else
 		lava-test-case mic-record-device-exist --result fail
 	fi
 
-	if [ -n $(arecord -l | grep $MIC) ]; then
+	if [ -n "$(echo "$ARECORD" | grep $MIC)" ]; then
 		lava-test-case arecord-mic-record-device-exist --result pass
 	else
 		lava-test-case arecord-mic-record-device-exist --result fail
@@ -88,13 +91,13 @@ fi
 
 # Line audio record
 if [ ! -z "$LINE" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $LINE) ]; then
+	if [ -n "$(echo "$CARDS" | grep $LINE)" ]; then
 		lava-test-case line-record-device-exist --result pass
 	else
 		lava-test-case line-record-device-exist --result fail
 	fi
 
-	if [ -n $(arecord -l | grep $LINE) ]; then
+	if [ -n "$(echo "$ARECORD" | grep $LINE)" ]; then
 		lava-test-case arecord-line-record-device-exist --result pass
 	else
 		lava-test-case arecord-line-record-device-exist --result fail
@@ -103,13 +106,13 @@ fi
 
 # USB audio record
 if [ ! -z "$USB_REC" ]; then
-	if [ -n $(cat /proc/asound/cards | grep $USB_REC) ]; then
+	if [ -n "$(echo "$CARDS" | grep $USB_REC)" ]; then
 		lava-test-case usb-record-device-exist --result pass
 	else
 		lava-test-case usb-record-device-exist --result fail
 	fi
 
-	if [ -n $(arecord -l | grep $USB_REC) ]; then
+	if [ -n "$(echo "$ARECORD" | grep $USB_REC)" ]; then
 		lava-test-case arecord-usb-record-device-exist --result pass
 	else
 		lava-test-case arecord-usb-record-device-exist --result fail
